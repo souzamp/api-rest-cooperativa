f74e0d1c8b9aa0d2257614358c24b2d47c46849f
Marcos paulo <marcos.souza.anl@gmail.com>
2021-03-06 22:56:38 -0300

    Projeto criado

---
+++ src/main/java/com/cooperativa/ApiRestCooperativaApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCooperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCooperativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooperativa/ApiRestCooperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCooperativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
