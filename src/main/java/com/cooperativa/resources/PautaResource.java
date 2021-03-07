84c15f4f76161e48e2842862b6eeccab7569dd12
Marcos paulo <marcos.souza.anl@gmail.com>
2021-03-07 01:36:40 -0300

    Criação do Repository e Service para Pauta

--- src/main/java/com/cooperativa/resources/PautaResource.java
+++ src/main/java/com/cooperativa/resources/PautaResource.java
@@ -1,26 +1,25 @@
 package com.cooperativa.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cooperativa.domain.Pauta;
+import com.cooperativa.services.PautaService;
 
 @RestController
 @RequestMapping(value = "/pautas")
 public class PautaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Pauta> pautas() {
+	@Autowired
+	private PautaService service;
 
-		Pauta pauta1 = new Pauta(1, "Seção para  aprovar orçamento 2021.");
-
-		List<Pauta> lista = new ArrayList<>();
-		lista.add(pauta1);
-
-		return lista;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
+		Pauta pauta = service.findPauta(id);
+		return ResponseEntity.ok().body(pauta);
 	}
 }
---
+++ src/main/java/com/cooperativa/services/PautaService.java
@@ -0,0 +1,21 @@
+package com.cooperativa.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cooperativa.domain.Pauta;
+import com.cooperativa.repositories.PautaRepository;
+
+@Service
+public class PautaService {
+
+	@Autowired
+	private PautaRepository pautaRepository;
+
+	public Pauta findPauta(Integer id) {
+		Optional<Pauta> findById = pautaRepository.findById(id);
+		return findById.orElse(null);
+	}
+}
